0bb19c2b7c5c83e98931d3f8bbb53500765248e3
WanYuLin <linwy@ntu.edu.tw>
2024-05-16 20:45:18 +0800

    Update MST.R

--- MST.R
+++ MST.R
@@ -3,8 +3,12 @@
 
 
 MST1 <- function(Traits, Covariate, SNP){
-    reg   <- lm(as.matrix(Traits) ~ as.factor(SNP) + as.matrix(Covariate), na.action = na.exclude)
-    dis <- apply(residuals(reg),2,function(x){(x-median(x,na.rm=T))^2})
+    residu   <- residuals(lm(as.matrix(Traits) ~ as.factor(SNP) + as.matrix(Covariate), na.action = na.exclude))
+    for(k in 1:ncol(residu)){
+        tmp <- which((residu[,k]>mean(residu[,k],na.rm=T)+5*sd(residu[,k],na.rm=T)) | (residu[,k]<mean(residu[,k],na.rm=T)-5*sd(residu[,k],na.rm=T)))
+        residu[tmp,k] <- NA
+        }              
+    dis <- apply(residu,2,function(x){(x-median(x,na.rm=T))^2})
     H1.model <- manova(dis ~ as.factor(SNP))
     H0.model <- manova(dis ~ 1)
     p.value <- matrix(NA,1, (ncol(Traits)+1))
